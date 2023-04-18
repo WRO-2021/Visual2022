6be0efb1a72f5fa72b3ab9886cf3dca22b16c11a
Samuele Facenda <samuele.facenda@gmail.com>
2023-04-18 11:42:00 +0200

    small cnn tests

--- ocr/neuralnetwork/CNN.py
+++ ocr/neuralnetwork/CNN.py
@@ -9,10 +9,12 @@ class NeuralNetwork(nn.Module):
             nn.Conv2d(in_channels=1 if not_color else 3, out_channels=20, kernel_size=5),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.ReLU(inplace=True),
             # second 2D convolution layer
             nn.Conv2d(in_channels=20, out_channels=50, kernel_size=5),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.ReLU(inplace=True),
         )
 
         self.linear_layers = nn.Sequential(
--- ocr/neuralnetwork/SmallCnn.py
+++ ocr/neuralnetwork/SmallCnn.py
@@ -5,18 +5,18 @@ class NeuralNetwork(nn.Module):
     def __init__(self, numLabel, not_color):
         super(NeuralNetwork, self).__init__()
         self.linear_relu_stack = nn.Sequential(
-            # Defining a 2D convolution layer
+            # image downsampled by 4
             nn.AvgPool2d(kernel_size=4, stride=4),
             nn.Conv2d(in_channels=1 if not_color else 3, out_channels=10, kernel_size=3),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2)
+            nn.Conv2d(in_channels=10, out_channels=32, kernel_size=5),
         )
 
         self.flatten = nn.Flatten(start_dim=1)
 
         self.linear_layers = nn.Sequential(
             # https://madebyollin.github.io/convnet-calculator/
-            nn.Linear(9, numLabel),
+            nn.Linear(32, numLabel),
             nn.LogSoftmax(dim=1)
         )
     def forward(self, x):
